a67f10e9fffcac9470e6d8d49a650b6569a70cca
Pranas Varaneckas <varaneckas@gmail.com>
2020-02-06 11:02:55 +0200

    Group DTO refactored

--- back/src/main/java/lt/vtmc/groups/dto/GroupDetailsDTO.java
+++ back/src/main/java/lt/vtmc/groups/dto/GroupDetailsDTO.java
@@ -1,6 +1,5 @@
 package lt.vtmc.groups.dto;
 
-
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,25 +10,28 @@ import lt.vtmc.groups.model.Group;
 import lt.vtmc.user.model.User;
 
 public class GroupDetailsDTO {
-	
+
 	private String name;
-	
+
 	private String description;
-	
+
+	private String[] userList;
+
 	@JsonIgnore
-	private List<User> userList;
-	
+	private List<User> userListArray;
+
 	@JsonIgnore
 	private List<DocType> docTypesToCreate;
-	
+
 	@JsonIgnore
 	private List<DocType> docTypesToApprove;
+
 	/**
 	 * Constructor method for GroupDetails
 	 * 
 	 * @param name
 	 * @param description
-	 * @param userList
+	 * @param userListArray
 	 * @param docTypesToCreate
 	 * @param docTypesToApprove
 	 */
@@ -38,66 +40,81 @@ public class GroupDetailsDTO {
 		super();
 		this.name = group.getName();
 		this.description = group.getDescription();
-		this.userList = group.getUserList();
+		this.userListArray = group.getUserList();
 		this.docTypesToCreate = group.getDocTypesToCreate();
 		this.docTypesToApprove = group.getDocTypesToApprove();
+		this.userList = getUserList();
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	public List<User> getUserList() {
-		return userList;
+
+	public List<User> getUserListArray() {
+		return userListArray;
 	}
-	public String[] getUserListNames() {
-		String[]userListNames = new String[userList.size()];
-		for (int i = 0; i < userList.size(); i++) {
-			userListNames[i] = userList.get(i).getUsername();
+
+	public String[] getUserList() {
+		String[] userListNames = new String[userListArray.size()];
+		for (int i = 0; i < userListArray.size(); i++) {
+			userListNames[i] = userListArray.get(i).getUsername();
 		}
 		return userListNames;
 	}
+
 	public void setUserList(List<User> userList) {
-		this.userList = userList;
+		this.userListArray = userList;
 	}
+
 	public List<DocType> getDocTypesToCreate() {
 		return docTypesToCreate;
 	}
+
 	public String[] getDocTypesToCreateNames() {
-		String[]docTypesNames = new String[userList.size()];
+		String[] docTypesNames = new String[userListArray.size()];
 		for (int i = 0; i < docTypesToCreate.size(); i++) {
 			docTypesNames[i] = docTypesToCreate.get(i).getDocumentType();
 		}
 		return docTypesNames;
 	}
+
 	public void setDocTypesToCreate(List<DocType> docTypesToCreate) {
 		this.docTypesToCreate = docTypesToCreate;
 	}
+
 	public List<DocType> getDocTypesToApprove() {
 		return docTypesToApprove;
 	}
+
 	public String[] getDocTypesToApproveNames() {
-		String[]docTypesNames = new String[userList.size()];
+		String[] docTypesNames = new String[userListArray.size()];
 		for (int i = 0; i < docTypesToApprove.size(); i++) {
 			docTypesNames[i] = docTypesToApprove.get(i).getDocumentType();
 		}
 		return docTypesNames;
 	}
+
 	public void setDocTypesToApprove(List<DocType> docTypesToApprove) {
 		this.docTypesToApprove = docTypesToApprove;
 	}
+
 	@Override
 	public String toString() {
-		return "[name: " + name + ", description: " + getDescription() + ", users: " + Arrays.toString(getUserListNames())
-				+ ", Can create: " + Arrays.toString(getDocTypesToCreateNames()) + ", Can approve: " + Arrays.toString(getDocTypesToApproveNames()) + "]";
+		return "[name: " + name + ", description: " + getDescription() + ", users: " + Arrays.toString(getUserList())
+				+ ", Can create: " + Arrays.toString(getDocTypesToCreateNames()) + ", Can approve: "
+				+ Arrays.toString(getDocTypesToApproveNames()) + "]";
 	}
-	
-	
+
 }
