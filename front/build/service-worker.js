c57f14633d78c5f85ba69ff23516770a044581f0
Pranas Varaneckas <varaneckas@gmail.com>
2020-03-27 09:49:39 +0200

    Added db example with log file

--- front/build/service-worker.js
+++ front/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.b591bfee36abdaede6305cfb5545b50f.js"
+  "./precache-manifest.5cbc952b8af5ff69ef45d55d08121491.js"
 );
 
 self.addEventListener('message', (event) => {
